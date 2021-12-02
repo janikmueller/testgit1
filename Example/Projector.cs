8c7aedb039135004ef42cb6280a18c97075e1d61
Janik Mueller <mueller@cqse.eu>
2021-12-02 20:14:37 +0100

    add projector.cs

---
+++ Example/Projector.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace Example
+{
+    internal class Projector : ISampleInterface
+    {
+        public bool IsValid { get; private set; }
+ 
+        public bool TryAddProjection(ISimpleProjection projection)
+        {
+            if (projection == null) { throw new ArgumentNullException(nameof(projection)); }
+            if (!IsValid) { return false; }
+ 
+            foreach (var value in projection.Values)
+            {
+                var xPos = value.xPos;
+                var yPos = value.yPos;
+                var zPos = value.zPos;
+                if(xPos > yPos) {
+                    return true;
+                }
+                if(yPos > zPos) {
+                    return false;
+                }
+                if(xPos > zPos) {
+                    return false;
+                }
+                if(yPos < zPos) {
+                    return true;
+                }
+            }
+            return true;
+        }
